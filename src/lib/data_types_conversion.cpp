347186127bcf99eeb63c6e86ca1fe5b3acad7b29
Mateusz Dukat <d3s.prv@mail.ru>
2017-12-31 21:17:40 +0100

    Add files via upload

---
+++ src/lib/data_types_conversion.cpp
@@ -0,0 +1,777 @@
+#include "data_types_conversion.h"
+namespace d3s{
+	int bintoint(char data){
+		switch(data){
+			case '\x00':
+				return 0;
+				break;
+			case '\x01':
+				return 1;
+				break;
+			case '\x02':
+				return 2;
+				break;
+			case '\x03':
+				return 3;
+				break;
+			case '\x04':
+				return 4;
+				break;
+			case '\x05':
+				return 5;
+				break;
+			case '\x06':
+				return 6;
+				break;
+			case '\x07':
+				return 7;
+				break;
+			case '\x08':
+				return 8;
+				break;
+			case '\x09':
+				return 9;
+				break;
+			case '\x0a':
+				return 10;
+				break;
+			case '\x0b':
+				return 11;
+				break;
+			case '\x0c':
+				return 12;
+				break;
+			case '\x0d':
+				return 13;
+				break;
+			case '\x0e':
+				return 14;
+				break;
+			case '\x0f':
+				return 15;
+				break;
+			case '\x10':
+				return 16;
+				break;
+			case '\x11':
+				return 17;
+				break;
+			case '\x12':
+				return 18;
+				break;
+			case '\x13':
+				return 19;
+				break;
+			case '\x14':
+				return 20;
+				break;
+			case '\x15':
+				return 21;
+				break;
+			case '\x16':
+				return 22;
+				break;
+			case '\x17':
+				return 23;
+				break;
+			case '\x18':
+				return 24;
+				break;
+			case '\x19':
+				return 25;
+				break;
+			case '\x1a':
+				return 26;
+				break;
+			case '\x1b':
+				return 27;
+				break;
+			case '\x1c':
+				return 28;
+				break;
+			case '\x1d':
+				return 29;
+				break;
+			case '\x1e':
+				return 30;
+				break;
+			case '\x1f':
+				return 31;
+				break;
+			case '\x20':
+				return 32;
+				break;
+			case '\x21':
+				return 33;
+				break;
+			case '\x22':
+				return 34;
+				break;
+			case '\x23':
+				return 35;
+				break;
+			case '\x24':
+				return 36;
+				break;
+			case '\x25':
+				return 37;
+				break;
+			case '\x26':
+				return 38;
+				break;
+			case '\x27':
+				return 39;
+				break;
+			case '\x28':
+				return 40;
+				break;
+			case '\x29':
+				return 41;
+				break;
+			case '\x2a':
+				return 42;
+				break;
+			case '\x2b':
+				return 43;
+				break;
+			case '\x2c':
+				return 44;
+				break;
+			case '\x2d':
+				return 45;
+				break;
+			case '\x2e':
+				return 46;
+				break;
+			case '\x2f':
+				return 47;
+				break;
+			case '\x30':
+				return 48;
+				break;
+			case '\x31':
+				return 49;
+				break;
+			case '\x32':
+				return 50;
+				break;
+			case '\x33':
+				return 51;
+				break;
+			case '\x34':
+				return 52;
+				break;
+			case '\x35':
+				return 53;
+				break;
+			case '\x36':
+				return 54;
+				break;
+			case '\x37':
+				return 55;
+				break;
+			case '\x38':
+				return 56;
+				break;
+			case '\x39':
+				return 57;
+				break;
+			case '\x3a':
+				return 58;
+				break;
+			case '\x3b':
+				return 59;
+				break;
+			case '\x3c':
+				return 60;
+				break;
+			case '\x3d':
+				return 61;
+				break;
+			case '\x3e':
+				return 62;
+				break;
+			case '\x3f':
+				return 63;
+				break;
+			case '\x40':
+				return 64;
+				break;
+			case '\x41':
+				return 65;
+				break;
+			case '\x42':
+				return 66;
+				break;
+			case '\x43':
+				return 67;
+				break;
+			case '\x44':
+				return 68;
+				break;
+			case '\x45':
+				return 69;
+				break;
+			case '\x46':
+				return 70;
+				break;
+			case '\x47':
+				return 71;
+				break;
+			case '\x48':
+				return 72;
+				break;
+			case '\x49':
+				return 73;
+				break;
+			case '\x4a':
+				return 74;
+				break;
+			case '\x4b':
+				return 75;
+				break;
+			case '\x4c':
+				return 76;
+				break;
+			case '\x4d':
+				return 77;
+				break;
+			case '\x4e':
+				return 78;
+				break;
+			case '\x4f':
+				return 79;
+				break;
+			case '\x50':
+				return 80;
+				break;
+			case '\x51':
+				return 81;
+				break;
+			case '\x52':
+				return 82;
+				break;
+			case '\x53':
+				return 83;
+				break;
+			case '\x54':
+				return 84;
+				break;
+			case '\x55':
+				return 85;
+				break;
+			case '\x56':
+				return 86;
+				break;
+			case '\x57':
+				return 87;
+				break;
+			case '\x58':
+				return 88;
+				break;
+			case '\x59':
+				return 89;
+				break;
+			case '\x5a':
+				return 90;
+				break;
+			case '\x5b':
+				return 91;
+				break;
+			case '\x5c':
+				return 92;
+				break;
+			case '\x5d':
+				return 93;
+				break;
+			case '\x5e':
+				return 94;
+				break;
+			case '\x5f':
+				return 95;
+				break;
+			case '\x60':
+				return 96;
+				break;
+			case '\x61':
+				return 97;
+				break;
+			case '\x62':
+				return 98;
+				break;
+			case '\x63':
+				return 99;
+				break;
+			case '\x64':
+				return 100;
+				break;
+			case '\x65':
+				return 101;
+				break;
+			case '\x66':
+				return 102;
+				break;
+			case '\x67':
+				return 103;
+				break;
+			case '\x68':
+				return 104;
+				break;
+			case '\x69':
+				return 105;
+				break;
+			case '\x6a':
+				return 106;
+				break;
+			case '\x6b':
+				return 107;
+				break;
+			case '\x6c':
+				return 108;
+				break;
+			case '\x6d':
+				return 109;
+				break;
+			case '\x6e':
+				return 110;
+				break;
+			case '\x6f':
+				return 111;
+				break;
+			case '\x70':
+				return 112;
+				break;
+			case '\x71':
+				return 113;
+				break;
+			case '\x72':
+				return 114;
+				break;
+			case '\x73':
+				return 115;
+				break;
+			case '\x74':
+				return 116;
+				break;
+			case '\x75':
+				return 117;
+				break;
+			case '\x76':
+				return 118;
+				break;
+			case '\x77':
+				return 119;
+				break;
+			case '\x78':
+				return 120;
+				break;
+			case '\x79':
+				return 121;
+				break;
+			case '\x7a':
+				return 122;
+				break;
+			case '\x7b':
+				return 123;
+				break;
+			case '\x7c':
+				return 124;
+				break;
+			case '\x7d':
+				return 125;
+				break;
+			case '\x7e':
+				return 126;
+				break;
+			case '\x7f':
+				return 127;
+				break;
+			case '\x80':
+				return 128;
+				break;
+			case '\x81':
+				return 129;
+				break;
+			case '\x82':
+				return 130;
+				break;
+			case '\x83':
+				return 131;
+				break;
+			case '\x84':
+				return 132;
+				break;
+			case '\x85':
+				return 133;
+				break;
+			case '\x86':
+				return 134;
+				break;
+			case '\x87':
+				return 135;
+				break;
+			case '\x88':
+				return 136;
+				break;
+			case '\x89':
+				return 137;
+				break;
+			case '\x8a':
+				return 138;
+				break;
+			case '\x8b':
+				return 139;
+				break;
+			case '\x8c':
+				return 140;
+				break;
+			case '\x8d':
+				return 141;
+				break;
+			case '\x8e':
+				return 142;
+				break;
+			case '\x8f':
+				return 143;
+				break;
+			case '\x90':
+				return 144;
+				break;
+			case '\x91':
+				return 145;
+				break;
+			case '\x92':
+				return 146;
+				break;
+			case '\x93':
+				return 147;
+				break;
+			case '\x94':
+				return 148;
+				break;
+			case '\x95':
+				return 149;
+				break;
+			case '\x96':
+				return 150;
+				break;
+			case '\x97':
+				return 151;
+				break;
+			case '\x98':
+				return 152;
+				break;
+			case '\x99':
+				return 153;
+				break;
+			case '\x9a':
+				return 154;
+				break;
+			case '\x9b':
+				return 155;
+				break;
+			case '\x9c':
+				return 156;
+				break;
+			case '\x9d':
+				return 157;
+				break;
+			case '\x9e':
+				return 158;
+				break;
+			case '\x9f':
+				return 159;
+				break;
+			case '\xa0':
+				return 160;
+				break;
+			case '\xa1':
+				return 161;
+				break;
+			case '\xa2':
+				return 162;
+				break;
+			case '\xa3':
+				return 163;
+				break;
+			case '\xa4':
+				return 164;
+				break;
+			case '\xa5':
+				return 165;
+				break;
+			case '\xa6':
+				return 166;
+				break;
+			case '\xa7':
+				return 167;
+				break;
+			case '\xa8':
+				return 168;
+				break;
+			case '\xa9':
+				return 169;
+				break;
+			case '\xaa':
+				return 170;
+				break;
+			case '\xab':
+				return 171;
+				break;
+			case '\xac':
+				return 172;
+				break;
+			case '\xad':
+				return 173;
+				break;
+			case '\xae':
+				return 174;
+				break;
+			case '\xaf':
+				return 175;
+				break;
+			case '\xb0':
+				return 176;
+				break;
+			case '\xb1':
+				return 177;
+				break;
+			case '\xb2':
+				return 178;
+				break;
+			case '\xb3':
+				return 179;
+				break;
+			case '\xb4':
+				return 180;
+				break;
+			case '\xb5':
+				return 181;
+				break;
+			case '\xb6':
+				return 182;
+				break;
+			case '\xb7':
+				return 183;
+				break;
+			case '\xb8':
+				return 184;
+				break;
+			case '\xb9':
+				return 185;
+				break;
+			case '\xba':
+				return 186;
+				break;
+			case '\xbb':
+				return 187;
+				break;
+			case '\xbc':
+				return 188;
+				break;
+			case '\xbd':
+				return 189;
+				break;
+			case '\xbe':
+				return 190;
+				break;
+			case '\xbf':
+				return 191;
+				break;
+			case '\xc0':
+				return 192;
+				break;
+			case '\xc1':
+				return 193;
+				break;
+			case '\xc2':
+				return 194;
+				break;
+			case '\xc3':
+				return 195;
+				break;
+			case '\xc4':
+				return 196;
+				break;
+			case '\xc5':
+				return 197;
+				break;
+			case '\xc6':
+				return 198;
+				break;
+			case '\xc7':
+				return 199;
+				break;
+			case '\xc8':
+				return 200;
+				break;
+			case '\xc9':
+				return 201;
+				break;
+			case '\xca':
+				return 202;
+				break;
+			case '\xcb':
+				return 203;
+				break;
+			case '\xcc':
+				return 204;
+				break;
+			case '\xcd':
+				return 205;
+				break;
+			case '\xce':
+				return 206;
+				break;
+			case '\xcf':
+				return 207;
+				break;
+			case '\xd0':
+				return 208;
+				break;
+			case '\xd1':
+				return 209;
+				break;
+			case '\xd2':
+				return 210;
+				break;
+			case '\xd3':
+				return 211;
+				break;
+			case '\xd4':
+				return 212;
+				break;
+			case '\xd5':
+				return 213;
+				break;
+			case '\xd6':
+				return 214;
+				break;
+			case '\xd7':
+				return 215;
+				break;
+			case '\xd8':
+				return 216;
+				break;
+			case '\xd9':
+				return 217;
+				break;
+			case '\xda':
+				return 218;
+				break;
+			case '\xdb':
+				return 219;
+				break;
+			case '\xdc':
+				return 220;
+				break;
+			case '\xdd':
+				return 221;
+				break;
+			case '\xde':
+				return 222;
+				break;
+			case '\xdf':
+				return 223;
+				break;
+			case '\xe0':
+				return 224;
+				break;
+			case '\xe1':
+				return 225;
+				break;
+			case '\xe2':
+				return 226;
+				break;
+			case '\xe3':
+				return 227;
+				break;
+			case '\xe4':
+				return 228;
+				break;
+			case '\xe5':
+				return 229;
+				break;
+			case '\xe6':
+				return 230;
+				break;
+			case '\xe7':
+				return 231;
+				break;
+			case '\xe8':
+				return 232;
+				break;
+			case '\xe9':
+				return 233;
+				break;
+			case '\xea':
+				return 234;
+				break;
+			case '\xeb':
+				return 235;
+				break;
+			case '\xec':
+				return 236;
+				break;
+			case '\xed':
+				return 237;
+				break;
+			case '\xee':
+				return 238;
+				break;
+			case '\xef':
+				return 239;
+				break;
+			case '\xf0':
+				return 240;
+				break;
+			case '\xf1':
+				return 241;
+				break;
+			case '\xf2':
+				return 242;
+				break;
+			case '\xf3':
+				return 243;
+				break;
+			case '\xf4':
+				return 244;
+				break;
+			case '\xf5':
+				return 245;
+				break;
+			case '\xf6':
+				return 246;
+				break;
+			case '\xf7':
+				return 247;
+				break;
+			case '\xf8':
+				return 248;
+				break;
+			case '\xf9':
+				return 249;
+				break;
+			case '\xfa':
+				return 250;
+				break;
+			case '\xfb':
+				return 251;
+				break;
+			case '\xfc':
+				return 252;
+				break;
+			case '\xfd':
+				return 253;
+				break;
+			case '\xfe':
+				return 254;
+				break;
+			case '\xff':
+				return 255;
+				break;
+			default:
+				return 0;
+		}
+	}
+}
